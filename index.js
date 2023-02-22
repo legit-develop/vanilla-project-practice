8066023ed8746559b0361a5fcfd9ca7213f627eb
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-22 15:33:17 +0200

    project reset

--- index.js
+++
@@ -1,7 +0,0 @@
-const xPositionEl = document.getElementById("x-position");
-const yPositionEl = document.getElementById("y-position");
-
-window.addEventListener("mousemove", (event) => {
-  xPositionEl.innerText = event.clientX;
-  yPositionEl.innerText = event.clientY;
-});
