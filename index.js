cc19552efb7cb9c4f7d6e32dd3e657a69a3f2f49
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-23 07:35:25 +0200

    new project version

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const counterEl = document.querySelector(".counter");
-const barEl = document.querySelector(".loading-bar-front");
 
-let barPercentage = 0;
-
-function updateNum() {
-  counterEl.innerText = `${barPercentage}%`;
-  barEl.style.width = `${barPercentage}%`;
-  barPercentage++;
-  if (barPercentage < 101) {
-    setTimeout(updateNum, 40);
-  }
-}
-
-updateNum();
