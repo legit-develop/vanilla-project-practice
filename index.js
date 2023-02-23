ea69f142e570a547978873fe79ca5663a9780ea2
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-23 07:56:05 +0200

    add loading bar functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const counterEl = document.querySelector(".counter");
+console.log(counterEl);
+const barEl = document.querySelector(".front-bar");
+console.log(barEl);
 
+let percentCounter = 0;
+
+function updateNumber() {
+  counterEl.innerText = `${percentCounter}%`;
+  barEl.style.width = `${percentCounter}%`;
+  percentCounter++;
+
+  if (percentCounter < 101) {
+    setTimeout(updateNumber, 40);
+  }
+}
+
+updateNumber();
