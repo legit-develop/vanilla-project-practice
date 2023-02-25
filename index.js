5b9217cdc0005df3b38f83163c01031713ba1441
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-25 06:03:39 +0200

    set element variable

---
+++ index.js
@@ -0,0 +1,5 @@
+const counterEl = document.getElementById("counter");
+const backBarEl = document.getElementById("back-bar");
+const frontBarEl = document.getElementById("front-bar");
+
+let percentCounter = 0;
