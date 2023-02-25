d6f299fe64d4d085e6bf6dc05beb7547178183b9
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-25 06:22:20 +0200

    complete the bar loading functionality

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const backBarEl = document.getElementById("back-bar");
 const frontBarEl = document.getElementById("front-bar");
 
 let percentCounter = 0;
+
+function updateCounter() {
+  counterEl.innerText = `${percentCounter}%`;
+  frontBarEl.style.width = `${percentCounter}%`;
+  percentCounter++;
+
+  if (percentCounter < 101) {
+    setTimeout(updateCounter, 20);
+  }
+}
+
+updateCounter();
