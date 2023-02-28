5f4bbf6dd9fb85a443a7ae51a3c2c53482ea8b64
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-28 11:59:29 +0200

    create date object and use toLocalDatestring method to get day and month

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 const monthEl = document.getElementById("month");
-const dateEl = document.getElementById("date");
 const dayEl = document.getElementById("day");
+const dateEl = document.getElementById("date");
 const yearEl = document.getElementById("year");
+
+const date = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function updateCalendar() {
+  //monthEl.innerText = months[date.getMonth()];
+  //dayEl.innerText = weekdays[date.getDay()];
+  monthEl.innerText = date.toLocaleDateString("en", { month: "long" });
+  dayEl.innerText = date.toLocaleDateString("en", { weekday: "long" });
+  dateEl.innerText = date.getDate();
+  yearEl.innerText = date.getFullYear();
+}
+
+updateCalendar();
