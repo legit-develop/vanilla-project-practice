2f29c822fdbce802d30566365b757ae00bab0da3
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-02 11:58:52 +0200

    api details info

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const jokeEl = document.getElementById("joke");
-console.log(jokeEl);
+
 const btnEl = document.getElementById("btn");
-console.log(btnEl);
+
+const apiKey = "V7aqF5COHMcp3MVAmbpBMw==IB3tbbQ8XyWqC9M7";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api.key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
