61c3216651fead78f84199acc5139e5fb1ee9ed3
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-03-03 11:03:22 +0200

    set up html elements and api url

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const jokeEl = document.getElementById("joke");
-
+const jokeText = document.getElementById("joke");
+console.log(jokeText);
 const btnEl = document.getElementById("btn");
+console.log(btnEl);
 
 const apiKey = "V7aqF5COHMcp3MVAmbpBMw==IB3tbbQ8XyWqC9M7";
 
 const options = {
   method: "GET",
   headers: {
-    "X-Api.key": apiKey,
+    "X-API-Key": apiKey,
   },
 };
 
-const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const apiURL = "https://api.api-ninjas.com/v1/dadjookes?limit=1";
